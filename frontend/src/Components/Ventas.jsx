1bb7c455dc884181960b8b27682aee49eb0d223d
Dyegov <diego.valencr@gmail.com>
2021-09-16 22:45:17 -0500

    Precios con punto

--- frontend/src/Components/Ventas.jsx
+++ frontend/src/Components/Ventas.jsx
@@ -6,7 +6,7 @@ const Ventas = ({ ventas }) => {
   }, []);
 
   function currencyFormat(num) {
-    return "$" + num.toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+    return "$" + num.toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
   }
 
   return (
