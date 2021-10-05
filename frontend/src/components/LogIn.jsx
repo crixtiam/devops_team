e2b2f74fc2d381510c818049dae4525658b390e4
Dyegov <diego.valencr@gmail.com>
2021-10-05 10:55:49 -0500

    Agregué estilos al Login

--- frontend/src/components/LogIn.jsx
+++ frontend/src/components/LogIn.jsx
@@ -9,24 +9,30 @@ const LogIn = () => {
       <div className='d-flex justify-content-center'>
         <br />
         <form>
-          <label htmlFor='email'>Email</label>
+          <label htmlFor='email' style={{ fontWeight: 'normal' }}>
+            Email
+          </label>
           <br />
-          <input type='email' name='email' id='email' />
+          <input type='email' name='email' id='email' style={{ marginBottom: '15px' }} />
           <br />
-          <br />
-          <label htmlFor='password'>Password</label>
+          <label htmlFor='password' style={{ fontWeight: 'normal' }}>
+            Password
+          </label>
           <br />
           <input type='password' name='password' id='password' />
           <br />
           <br />
-          <button>Log in</button>
-          <br />
-          <br />
-          <div>Or continue with your Gmail account</div>
-          <img src='/google_sign_up.png' alt='Google' height='100px' />
-          <br />
-          <Link to='/'>Sign up</Link> | <Link to=''>Forgot Password</Link>
+          <button style={{ display: 'block', width: '100%' }}>Log in</button>
         </form>
+      </div>
+      <br />
+      <div className='d-flex justify-content-center'>Or continue with your Gmail account</div>
+      <div className='d-flex justify-content-center'>
+        <img src='/google_sign_up.png' alt='Google' height='100px' />
+        <br />
+      </div>
+      <div className='d-flex justify-content-center'>
+        <Link to='/'>Sign up</Link> | <Link to=''>Forgot Password</Link>
       </div>
     </div>
   );
